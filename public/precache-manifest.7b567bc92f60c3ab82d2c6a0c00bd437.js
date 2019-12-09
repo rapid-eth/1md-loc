b3bf69cba3b50eea9001b46f2afd20a381e4aff3
Pat <me@onpaws.com>
2019-12-09 17:18:30 +0100

    v1.0 of map widget

---
+++ public/precache-manifest.7b567bc92f60c3ab82d2c6a0c00bd437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ada9d0b14d9ced2c99f947e241ff791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21901b539ec1bf9d63d1",
+    "url": "/static/css/main.085f9294.chunk.css"
+  },
+  {
+    "revision": "c9304e77d6f13ed11a36",
+    "url": "/static/js/2.8905fbda.chunk.js"
+  },
+  {
+    "revision": "8d60b3ead87304143e2ccffd3ed23467",
+    "url": "/static/js/2.8905fbda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21901b539ec1bf9d63d1",
+    "url": "/static/js/main.bf95aed6.chunk.js"
+  },
+  {
+    "revision": "3a298ab4ed5ea7a1eb17",
+    "url": "/static/js/runtime-main.f01d1c78.js"
+  }
+]);
